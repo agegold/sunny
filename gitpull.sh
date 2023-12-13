389945cc3d3ce29bbbd6058bebb41be24af0314b
agegold <66872114+agegold@users.noreply.github.com>
2023-12-13 18:04:51 +0900

    Update gitpull.sh

--- gitpull.sh
+++ gitpull.sh
@@ -6,28 +6,10 @@ export PATH=/usr/local/bin:/data/data/com.termux/files/usr/bin:/data/data/com.te
 export PYTHONPATH=/data/openpilot
 
 cd /data/openpilot
-ping -q -c 1 -w 1 google.com &> /dev/null
-if [ "$?" == "0" ]; then
-  REMOVED_BRANCH=$(git branch -vv | grep ': gone]' | awk '{print $1}')
-  if [ "$REMOVED_BRANCH" != "" ]; then
-    if [ "$REMOVED_BRANCH" == "*" ]; then
-      REMOVED_BRANCH=$(git branch -vv | grep ': gone]' | awk '{print $2}')
-    fi
-    git remote prune origin --dry-run
-    echo $REMOVED_BRANCH | xargs git branch -D
-    sed -i "/$REMOVED_BRANCH/d" .git/config
-  fi
-  BRANCH=$(git rev-parse --abbrev-ref HEAD)
-  HASH=$(git rev-parse HEAD)
-  /data/data/com.termux/files/usr/bin/git fetch
-  REMOTE_HASH=$(git rev-parse --verify origin/$BRANCH)
-  /data/data/com.termux/files/usr/bin/git pull origin $BRANCH
-
-  if [ "$HASH" != "$REMOTE_HASH" ]; then
-    if [ -f "/data/openpilot/prebuilt" ]; then
-      pkill -f thermald
-      rm -f /data/openpilot/prebuilt
-    fi
-    reboot
-  fi
-fi
+echo -n "0" > /data/params/d/PutPrebuilt
+rm -f prebuilt
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+/data/data/com.termux/files/usr/bin/git fetch --all
+#/data/data/com.termux/files/usr/bin/git pull
+REMOTE_HASH=$(git rev-parse --short --verify origin/$BRANCH)
+/data/data/com.termux/files/usr/bin/git reset --hard $REMOTE_HASH
